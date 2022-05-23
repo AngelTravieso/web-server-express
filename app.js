86ef346f5ec11a423d35f247d8266eb95bfba4c7
AngelTravieso <angeltraviesoc@gmail.com>
2022-05-22 21:12:34 -0400

    introduccion a express

---
+++ app.js
@@ -0,0 +1,52 @@
+// const express = require('express')
+// const app = express()
+// const port = 8080;
+
+// app.get('/', (req, res) => {
+//   res.send('Home page');
+// });
+
+// app.get('/hola-mundo', (req, res) => {
+//     res.send('Hola mundo en su respectiva ruta');
+// });
+
+// // Para cualquier otra ruta
+// app.get('*', (req, res) => {
+//     res.send('404 | Page not found');
+// });
+
+
+// app.listen(port, () => {
+//     console.log(`Example app listening on port ${port}`);
+// });
+  
+
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+    res.send('Home Page');
+});
+
+app.get('/hola-mundo', (req, res) => {
+    res.send('Hola mundo');
+});
+
+app.get('*', (req, res) => {
+    res.send('404 | Page not found');
+});
+
+app.listen( port, () => {
+    console.log(`Servidor corriendo en el puerto ${port}`);
+});
+
+
+
+
+
+
+
+
+
+
